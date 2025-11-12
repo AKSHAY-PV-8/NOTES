54457c9a349bae6886428adab4b6a3fbb300962c
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-12 12:19:03 +0530

    day8 q1

---
+++ test/day8.js
@@ -0,0 +1,25 @@
+const reverseUsingRecursion = (string, i = 0) => {
+    if(i === string.length) return ""
+    return reverseUsingRecursion(string, i+1) + string[i]
+}
+
+// console.log(reverseUsingRecursion("hellow"));
+
+function checkArrayAreEqual(arr1, arr2){
+
+    let set = {}
+
+    if(!Array.isArray(arr1) || !Array.isArray(arr2)) return undefined
+    if(arr1.length !== arr2.length) return false
+    
+    for(let i of arr1){
+        set[i] = (set[i] || 0) + 1
+    }
+    for(let i of arr2){
+        if(!set[i]) return false
+        set[i] --;
+    }
+    return true
+}
+
+// console.log(checkArrayAreEqual([1,2,3], [1,2,3]))
