390a83310ff743162b01c6dc05dad6fcc34f8e40
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-12 22:51:36 +0530

    test q2

--- test/test.js
+++ test/test.js
@@ -19,4 +19,39 @@ function reverse(string){
     return result
 
 }
-console.log(reverse("JavaScript"))
+// console.log(reverse("JavaScript"))
+
+function palindrome(str){
+
+    const isAlphabet = (string) => {
+          
+        const code = string.charCodeAt(0)
+
+        return(
+            (code >= 97 && code <= 122) ||
+            (code >= 65 && code <= 90)
+        )
+    }
+
+    let string = ''
+
+    for (let char of str){
+        if(isAlphabet(char)){
+            string += char
+        }
+    }
+
+    let left = 0, right = string.length - 1;
+
+    while(left < right){
+        if(string[left] != string[right]) return false
+        left ++
+        right --
+    }
+
+    return true
+}
+
+const text = "mom"
+
+console.log(palindrome(text))
