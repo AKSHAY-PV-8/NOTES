4799baf61af18cfe7b7042a302305427d8b257d5
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-14 13:47:11 +0530

    day10 q3 Closure(filter)

--- TypeScript.js
+++ TypeScript.js
@@ -1,4 +1,5 @@
 Type Script
+-----------
 
         type script is realy a super set of js
 
@@ -7,4 +8,22 @@ Type Script
 
             => type script is all about type safety
 
-            
+    => The only job that typescript done for you is 
+
+                => Static checking. 
+
+        sub) What is mean by static checking
+
+                checking code for error before running if. 
+
+
+                => typeScript uses static checking to detect.
+
+                    . type mistakes
+                    . missing properties
+                    . wrong retun values
+                    . invalid function arguments
+                    . code paths that can cause runtime errors
+                    
+
+                
--- test/day10.js
+++ test/day10.js
@@ -23,3 +23,21 @@ function counter(){
 
 // fun()
 // fun()
+
+function filter(arr){
+    const array = arr
+    let result = []
+    return (cod) => {
+        for(let i of array){
+             if(cod(i)){
+                result.push(i)
+             }
+        }
+        console.log(result)
+    }
+}
+
+// const fun = filter([1,2,3,4])
+// fun(n => n > 2)
+
+// filter([1,2,3,4])(n => n > 2) // [3,4]
