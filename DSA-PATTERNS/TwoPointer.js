6f83d1253727e91355aa4df72bece5bcb3b9abe1
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-19 14:42:49 +0530

    DSA 2-POINTER q2

--- DSA-PATTERNS/TwoPointer.js
+++ DSA-PATTERNS/TwoPointer.js
@@ -17,3 +17,21 @@ function TwoSum(arr, n){
 }
 
 console.log(TwoSum([2,7,11,15], 9)) //best way for sorted array
+
+function RemoveDuplicatesWithoutUsingExtraArray (arr){
+    let j = 1;
+    for(let i = 0; i < arr.length ; i++){
+        if(arr[i] != arr[j-1]){
+            arr[j] = arr[i]
+            j++
+        }
+    }
+
+    return j
+}
+
+console.log(RemoveDuplicatesWithoutUsingExtraArray([0,1,1,2,2,3]))
+
+
+
+
