a1ecb783a270f504ac6cba5818e10b3fc6914520
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-21 10:53:43 +0530

    completed React hook form

--- ReactHookForm/vite-project/src/assets/components/DynamicField.tsx
+++ ReactHookForm/vite-project/src/assets/components/DynamicField.tsx
@@ -1,4 +1,6 @@
 import { useForm, useFieldArray,type SubmitHandler } from "react-hook-form"
+import {z} from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 type DataType = {
     name: string,
@@ -6,16 +8,25 @@ type DataType = {
         number: string
     }[],
     RollNumber: number
+    DOB: Date
 }
+
+const schema = z.object({
+    name: z.string().nonempty("username is required")
+})
 
 const DynamicField = () => {
 
-    const { register, control, handleSubmit } = useForm<DataType>({
+    const { register, control, handleSubmit, watch, getValues, setValue, formState, trigger } = useForm<DataType>({
         defaultValues: {
             name: "batman",
             phNumbers: [{ number: "" }],
-            RollNumber: 0
-        }
+            RollNumber: 0,
+            DOB : new Date()
+        },
+        mode: "onBlur", // to trigger the validation when focus out the the input . there are option like "onSubmit", "onChange","onTouche","all"
+        resolver: zodResolver(schema)
+    
     })
 
     const { fields, append, remove } = useFieldArray({
@@ -23,7 +34,21 @@ const DynamicField = () => {
         control
     })
 
+    const {errors, touchedFields, dirtyFields} = formState
+
+    console.log(dirtyFields, touchedFields)
+
+    const watchUsername = watch("name")
+
     const onSubmit : SubmitHandler<DataType> = (data) => console.log(data)
+
+    const handleGetValues = () => {
+        console.log(getValues())
+    }
+    
+    const handleSetValues = () => {
+        setValue("name", "Akshay")
+    }
 
     return (
 
@@ -43,6 +68,7 @@ const DynamicField = () => {
                 }
             </div>
             <button onClick={() => append({number: ""})}>Add number</button>
+
             <input type="text" {...register("RollNumber",{
                 required: true,
                 valueAsNumber: true,
@@ -51,13 +77,31 @@ const DynamicField = () => {
                         fieldValue < 10 || "Enter a different Id"
                     )
                 }
+
         
+            })}/>
+            {errors.RollNumber && <span>enter valid rollnumber</span>}
+
+            <input type="date" {...register("DOB", {
+                required:true,
+                valueAsDate: true
             })}/>
 
             <button type="submit">submit</button>
-            </form>
-        </>
 
+            <button type="button" onClick={handleGetValues}>Get values</button>
+
+
+            <button type="button" onClick={handleSetValues}>Set values</button>
+
+             <button type="button" onClick={() => trigger()}>Trigger validation</button>
+
+            
+            </form>
+
+            <h1>watch userName: {watchUsername}</h1>
+
+        </>
     )
 }
 
