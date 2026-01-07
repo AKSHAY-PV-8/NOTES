d10877b1f616ab332bab9f735b03c8e66fcf9964
AKSHAY-PV-8 <akshaypv19@gmail.com>
2026-01-07 08:10:42 +0530

    notes

--- Interview/pract.js
+++ Interview/pract.js
@@ -836,3 +836,432 @@ function binarySearch(arr, target) {
 
     return -1; // not found
 }
+
+function nonRepeating(s, k) {
+    let left = 0;
+    let set = new Set()
+    let result = []
+
+    function returnNonRepeating(str) {
+        let map = new Map()
+        let last = str[str.length - 1]
+
+        for (let char of str) {
+            map.set(char, (map.get(char) || 0) + 1)
+        }
+        console.log(map)
+        console.log("seen", set)
+
+        for (let [value, count] of map) {
+            if (set.size === 0) {
+                set.add(value)
+                return value
+            }
+            else if (!set.has(value) && count == 1) {
+                set.add(value)
+                return value
+            }
+            else if (set.has(value) && count === 1 && value == last) {
+                return value
+            }
+        }
+
+    }
+
+    for (let right = 0; right < s.length; right++) {
+        while (right - left + 1 === k) {
+            let str = s.substring(left, right + 1)
+            console.log(str)
+            result.push(returnNonRepeating(str))
+            console.log("result", result)
+            left++
+        }
+
+    }
+    return result
+
+}
+// console.log(nonRepeating("aabcdbec", 4))
+
+class Heap {
+    constructor(condition) {
+        this.heap = []
+        this.comapare = condition
+    }
+    getParent(i) { return Math.floor((i - 1) / 2) }
+    getLeft(i) { return i * 2 + 1 }
+    getRight(i) { return i * 2 + 2 }
+    swap(i, j) { return [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]] }
+
+    add(value) {
+        this.heap.push(value)
+        this.heapifyUp()
+    }
+
+    remove() {
+        if (this.heap.length === 1) return this.heap.pop()
+
+        let node = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.heapifyDown()
+        return node
+    }
+    heapifyUp() {
+        let i = this.heap.length - 1;
+        while (i > 0) {
+            if (this.comapare(this.heap[i], this.heap[this.getParent(i)]) < 0) {
+                this.swap(i, this.getParent(i))
+                i = this.getParent(i)
+            } else break
+        }
+
+    }
+    heapifyDown() {
+        let i = 0;
+        let length = this.heap.length;
+
+        while (true) {
+            let left = this.getLeft(i)
+            let right = this.getRight(i)
+            let small = i
+
+            if (left < length && this.comapare(this.heap[left], this.heap[small]) < 0) small = left
+            if (right < length && this.comapare(this.heap[right], this.heap[small]) < 0) small = right
+
+            if (small !== i) {
+                this.swap(small, i)
+                i = small
+            } else break
+        }
+    }
+}
+
+function serverLoadBalaner(servers, tasks) {
+    let heap = new Heap((a, b) => a - b)
+
+    for (let i of servers) {
+        heap.add(i)
+    }
+
+    console.log(heap.heap)
+    for (let i of tasks) {
+        let server = heap.remove()
+        server += i
+        heap.add(server)
+    }
+
+    return heap.heap
+
+}
+// console.log(serverLoadBalaner([3, 3, 5], [4, 2, 1, 3]))
+
+
+function minSteps(height) {
+    height.sort((a, b) => a - b)
+
+    let mid = Math.floor(height.length / 2)
+    let top = height[height.length - 1]
+    let bottom = height[0]
+    console.log(top, bottom)
+    let midDiff = 0
+    let steps = 0
+
+    if (height.length % 2 === 0) {
+        midDiff = height[mid] - height[mid - 1]
+        steps = (height[mid - 1] - bottom) + (top - height[mid - 1]) + midDiff
+
+    } else {
+        midDiff = 0
+        steps = (height[mid] - bottom) + (top - height[mid]) + midDiff
+    }
+
+    return steps
+}
+// console.log(minSteps([1, 5, 4, 7]))
+
+
+function message(s) {
+
+    let result = ""
+    let track = ""
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i]
+
+        if (isNaN(char)) {
+            track += char
+        } else {
+            let count = Number(char)
+            for (let i = 0; i < count; i++) {
+                result += track
+            }
+            track = ""
+        }
+    }
+    return result
+
+}
+// console.log(message("a2b3cd2"))
+
+
+function logest(nums) {
+    let left = 0;
+    let right = 1
+    let count = 1
+    let maxLen = 0
+    let prev = nums[left]
+
+    while (left < nums.length) {
+        if (prev < nums[right]) {
+            prev = nums[right]
+            count++
+            right++
+        } else {
+            maxLen = Math.max(maxLen, count)
+            left++
+            count = 0
+            prev = left
+            right = left + 1
+        }
+
+    }
+
+    return maxLen
+}
+// console.log(logest([2, 2, 2, 2]))
+
+
+function MinimumWindow(s, t) {
+    let left = 0;
+    let minLen = Infinity
+    let start = 0
+    let map = new Map()
+    for (let char of t) {
+        map.set(char, (map.get(char) || 0) + 1)
+    }
+    let requierd = map.size
+
+    for (let right = 0; right < s.length; right++) {
+        let rightChar = s[right]
+        if (map.has(rightChar)) {
+            map.set(rightChar, map.get(rightChar) - 1)
+        }
+        if (map.get(rightChar) === 0) {
+            requierd--
+        }
+
+        while (requierd === 0) {
+            if (right - left + 1 < minLen) {
+                start = left
+                minLen = right - left + 1
+            }
+            let leftChar = s[left]
+            if (map.has(leftChar)) {
+                map.set(leftChar, (map.get(leftChar) || 0) + 1)
+            }
+            if (map.get(leftChar) > 0) {
+                requierd++
+            }
+            left++
+        }
+    }
+
+    return s.substring(start, start + minLen)
+
+}
+// console.log(MinimumWindow
+//     ("ADOBECODEBANC", "ABC"))
+
+
+function negation(nums, k) {
+    let heap = new Heap((a, b) => a - b)
+
+    for (let i of nums) {
+        heap.add(i)
+    }
+    for (let i = 0; i < k; i++) {
+        let pop = heap.remove()
+        heap.add(-(pop))
+        console.log("index:", i, heap.heap)
+    }
+    let result = 0
+
+    for (let i of heap.heap) {
+        result += i
+    }
+    return result
+}
+// console.log(negation([3, -1, 0, 2], 3))
+
+function firstMissingPosition(nums) {
+    nums.sort((a, b) => a - b)
+    if (nums[0] != 0) return 1
+    for (let i = 0; i < nums.length - 1; i++) {
+
+        if (nums[i] + 1 != nums[i + 1]) {
+            return nums[i + 1]
+        }
+    }
+    return nums[nums.length - 1] + 1
+
+}
+// console.log(firstMissingPosition([1, 2, 0]))
+
+function longestSUbstring(s) {
+    let left = 0;
+    let set = new Set()
+    let maxLen = 0
+
+    for (let right = 0; right < s.length; right++) {
+        let rightChar = s[right]
+
+        while (set.has(rightChar)) {
+            let leftChar = s[left]
+            maxLen = Math.max(maxLen, right - left)
+            set.delete(leftChar)
+            left++
+        }
+        set.add(rightChar)
+    }
+    return maxLen
+
+}
+// console.log(longestSUbstring("bbbbbb"))
+
+function islands(grid) {
+
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let count = 0
+
+    function dfs(r, c) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols) {
+            return false
+        }
+
+        if (grid[r][c] === 1) {
+            return true
+        }
+
+        grid[r][c] = 1;
+        let up = dfs(r - 1, c);
+        let down = dfs(r + 1, c);
+        let left = dfs(r, c - 1);
+        let right = dfs(r, c + 1);
+
+        return up && down && left && right;
+
+    }
+
+    for (let r = 0; r < rows; r++) {
+        for (c = 0; c < cols; c++) {
+            if (grid[r][c] === 0) {
+                if (dfs(r, c)) {
+                    count++
+                }
+
+            }
+        }
+    }
+
+    return count
+
+}
+// console.log(islands([
+//     [1, 1, 1, 1],
+//     [1, 0, 0, 1],
+//     [1, 0, 1, 1],
+//     [1, 1, 1, 1]
+// ]))
+
+
+function splitArray(nums) {
+    let totalSum = nums.reduce((a, b) => a + b, 0);
+    let prefixSum = 0;
+    let parts = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        prefixSum += nums[i];
+
+        // Try to make each part equal to prefixSum
+        if (totalSum % prefixSum === 0) {
+            let target = prefixSum;
+            let current = 0;
+            let count = 0;
+
+            for (let j = i + 1; j < nums.length; j++) {
+                current += nums[j];
+                if (current === target) {
+                    count++;
+                    current = 0;
+                } else if (current > target) {
+                    break;
+                }
+            }
+
+            if (current === 0) {
+                parts = Math.max(parts, count + 1);
+            }
+        }
+    }
+
+    return parts;
+}
+// console.log(splitArray([1,2,3,3]))
+
+function eauals(nums){
+    let map = new Map()
+    map.set(0, -1)
+    let sum = 0
+    let maxLen = 0
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i] === 0 ? -1 : 1;
+        if(map.has(sum)){
+            maxLen = Math.max(maxLen, i - map.get(sum))
+        }
+        else{
+            map.set(sum, i)
+        }
+
+        
+    }
+    return maxLen
+
+}
+// console.log(eauals([0,1,0,1]))
+
+function kthSmallPair(nums, k){
+    let heap = new Heap((a,b) => b - a)
+
+    let left = 0
+    let right = left + 1
+    let array = []
+
+
+    while(left < nums.length - 1){
+        let diff = 0
+        if(nums[left] >= nums[right]){
+            diff = nums[left] - nums[right]
+        }else{
+            diff = nums[right] - nums[left]
+        }
+        array.push(diff)
+        right ++
+
+        if(right == nums.length){
+            left ++
+            right = left + 1
+        }
+    }
+    
+    for(let num of array){
+        heap.add(num)
+        if(heap.heap.length > k){
+            heap.remove()
+        }
+    }
+
+    return heap.remove()
+
+}
+// console.log(kthSmallPair([1,3,1], 1))
